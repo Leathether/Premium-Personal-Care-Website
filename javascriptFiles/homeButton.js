f60be2bebf00c801ec965e401f67dbd81805090e
Peter Magenheim <pwmagenheim@gmail.com>
2024-05-23 03:43:10 -0400

    4.23.2024v10

--- javascriptFiles/homeButton.js
+++ javascriptFiles/homeButton.js
@@ -1,7 +1,7 @@
 document.getElementById("homeButton").onclick = function(){window.open("/Premium-Personal-Care-Website/", "_self"); console.log("Func Works");}
 console.log("homeButton.js is working new3")
-document.getElementById("ourMissionButton").onclick = function(){window.open("/ourMission", "_self");}
-document.getElementById("ourPurposeButton").onclick = function(){window.open("/ourPurpose", "_self");}
-document.getElementById("eventsButton").onclick = function(){window.open("/events", "_self");}
-document.getElementById("tripsButton").onclick = function(){window.open("/trips", "_self");}
-document.getElementById("contactButton").onclick = function(){window.open("/contact", "_self");}
+document.getElementById("ourMissionButton").onclick = function(){window.open("../ourMission", "_self");}
+document.getElementById("ourPurposeButton").onclick = function(){window.open("../ourPurpose", "_self");}
+document.getElementById("eventsButton").onclick = function(){window.open("../events", "_self");}
+document.getElementById("tripsButton").onclick = function(){window.open("../trips", "_self");}
+document.getElementById("contactButton").onclick = function(){window.open("../contact", "_self");}
