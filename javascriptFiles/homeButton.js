1a6f6d44332e4db0135b5acddcafa7c11ca184f4
Peter Magenheim <pwmagenheim@gmail.com>
2024-05-23 00:07:01 -0400

    5.23.2024v1

--- javascriptFiles/homeButton.js
+++ javascriptFiles/homeButton.js
@@ -1,2 +1,2 @@
-getElementsByClassName("homeButton").onClick = function() {window.open("../", "_self");}
-console.log("homeButton.js is working new2")
+document.getElementsByClassName("homeButton").onClick = function() {window.open("../", "_self");}
+console.log("homeButton.js is working new3")
