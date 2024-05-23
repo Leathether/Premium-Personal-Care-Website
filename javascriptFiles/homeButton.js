e5103d6dbab0b7959831f3aeccac5a2428122d6d
Peter Magenheim <pwmagenheim@gmail.com>
2024-05-23 04:00:00 -0400

    4.23.2024v11

--- javascriptFiles/homeButton.js
+++ javascriptFiles/homeButton.js
@@ -1,7 +1,7 @@
-document.getElementById("homeButton").onclick = function(){window.open("/Premium-Personal-Care-Website/", "_self"); console.log("Func Works");}
+document.getElementById("homeButton").onclick = function(){window.location.href = "/Premium-Personal-Care-Website/"; console.log("Func Works");}
 console.log("homeButton.js is working new3")
-document.getElementById("ourMissionButton").onclick = function(){window.open("../ourMission", "_self");}
-document.getElementById("ourPurposeButton").onclick = function(){window.open("../ourPurpose", "_self");}
-document.getElementById("eventsButton").onclick = function(){window.open("../events", "_self");}
-document.getElementById("tripsButton").onclick = function(){window.open("../trips", "_self");}
-document.getElementById("contactButton").onclick = function(){window.open("../contact", "_self");}
+document.getElementById("ourMissionButton").onclick = function(){window.location.href = "../ourMission"}
+document.getElementById("ourPurposeButton").onclick = function(){window.location.href = "../ourPurpose"}
+document.getElementById("eventsButton").onclick = function(){window.location.href = "../events"}
+document.getElementById("tripsButton").onclick = function(){window.location.href = "../trips"}
+document.getElementById("contactButton").onclick = function(){window.location.href = "..add/contact"}
