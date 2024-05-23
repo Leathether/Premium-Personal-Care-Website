950eff86827893cf57c02318c393bb951d293c3a
Peter Magenheim <pwmagenheim@gmail.com>
2024-05-23 01:48:45 -0400

    5.23.2024v8

--- javascriptFiles/homeButton.js
+++ javascriptFiles/homeButton.js
@@ -1,2 +1,2 @@
-document.getElementById("homeButton").onclick = function(){window.open("../", "_self"); console.log("Func Works");}
+document.getElementById("homeButton").onclick = function(){window.open("/Premium-Personal-Care-Website/", "_self"); console.log("Func Works");}
 console.log("homeButton.js is working new3")
